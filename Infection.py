e0e7e834ac469afb4c73110838b7028d55587500
Josh <joshuaj004@gmail.com>
2016-09-19 21:27:55 -0700

    Added some more basic methods to the User class so that coaches and trainees can be added.

--- Infection.py
+++ Infection.py
@@ -1,10 +1,14 @@
 # This is the main file that will use the Users class
-import User
+from User import User
 
 
 def main():
     print("Hello world!")
     matt = User()
+    print(matt)
+    print(matt.getCoaches())
+    print(matt.getTrainees())
+    print(matt.getVersion())
 
 if __name__ == "__main__":
     main()
--- User.py
+++ User.py
@@ -14,3 +14,31 @@ class User:
 
     def getVersion(self):
         return self.version
+
+    def addCoach(self, coach):
+        if len(self.coaches > 0):
+            raise Exception("You cannot at a coach to a user who already has a coach")
+        else:
+            self.coaches = coach
+
+    def removeCoach(self, coach):
+        if len(self.coaches < 1):
+            raise Exception("This user has no coaches")
+        elif coach not in self.coaches:
+            raise Exception("This user is not coached by this coach")
+        else:
+            self.coaches = []
+
+    def addTrainee(self, trainee):
+        if trainee in self.trainees:
+            raise Exception("This user is already coaching this trainee")
+        else:
+            self.trainees.append(trainee)
+
+    def removeTrainee(self, trainee):
+        if len(self.trainees < 1):
+            raise Exception("This user has no trainess")
+        elif trainee not in self.trainees:
+            raise Exception("This user is not coaching this trainee")
+        else:
+            self.trainees.remove(trainee)
