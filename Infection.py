c9ced9f2bc91ce1050f41458dcd9e8bd7f08a9a6
Josh <joshuaj004@gmail.com>
2016-09-19 23:42:54 -0700

    Made some changes to the User class by adding a string representation and added a tester method to the infection program.

--- Infection.py
+++ Infection.py
@@ -3,12 +3,33 @@ from User import User
 
 
 def main():
-    print("Hello world!")
-    matt = User()
-    print(matt)
-    print(matt.getCoaches())
-    print(matt.getTrainees())
-    print(matt.getVersion())
+    tester()
+
+
+def tester():
+    matt = User("matt")
+    sarah = User("sarah")
+    james = User("james")
+    john = User("john")
+    kylie = User("kylie")
+    chris = User("chris")
+    users = [matt, sarah, james, john, kylie, chris]
+    # Matt coaches Sarah, James, and John
+    matt.addTrainee(sarah)
+    matt.addTrainee(james)
+    matt.addTrainee(john)
+    # Kylie coaches Chris
+    kylie.addTrainee(chris)
+    printer(users)
+    print("*"*50)
+    # # John leaves Matt to be coached by Kylie
+    john.removeCoach(matt)
+    kylie.addTrainee(john)
+    printer(users)
+
+def printer(users):
+    for x in users:
+        print(x)
 
 if __name__ == "__main__":
     main()
--- User.py
+++ User.py
@@ -1,10 +1,14 @@
 # Spec: https://docs.google.com/document/d/1NiKv-MjULOFyyc8f5w8R_EqvuPJ10wJVJgZhtTK9VKc
 class User:
     """A simple User class for the infection scenario"""
-    def __init__(self, coaches=[], trainees=[], version=0):
-        self.coaches = coaches
-        self.trainees = trainees
-        self.version = version
+    def __init__(self, name):#, coaches=[], trainees=[], version=0):
+        self.name = name
+        self.coaches = []#coaches
+        self.trainees = []#trainees
+        self.version = 0#version
+
+    def getName(self):
+        return self.name
 
     def getCoaches(self):
         return self.coaches
@@ -15,30 +19,46 @@ class User:
     def getVersion(self):
         return self.version
 
-    def addCoach(self, coach):
-        if len(self.coaches > 0):
+    def addCoach(self, coach, auto=False):
+        if len(self.coaches) > 0:
             raise Exception("You cannot at a coach to a user who already has a coach")
         else:
-            self.coaches = coach
+            self.coaches = [coach]
+            if not auto:
+                coach.addTrainee(self, True)
 
-    def removeCoach(self, coach):
-        if len(self.coaches < 1):
+    def removeCoach(self, coach, auto=False):
+        if len(self.coaches) < 1:
             raise Exception("This user has no coaches")
         elif coach not in self.coaches:
             raise Exception("This user is not coached by this coach")
         else:
             self.coaches = []
+            if not auto:
+                coach.removeTrainee(self, True)
 
-    def addTrainee(self, trainee):
+    def addTrainee(self, trainee, auto=False):
         if trainee in self.trainees:
             raise Exception("This user is already coaching this trainee")
         else:
             self.trainees.append(trainee)
+            if not auto:
+                trainee.addCoach(self, True)
 
-    def removeTrainee(self, trainee):
-        if len(self.trainees < 1):
+
+    def removeTrainee(self, trainee, auto=False):
+        if len(self.trainees) < 1:
             raise Exception("This user has no trainess")
         elif trainee not in self.trainees:
             raise Exception("This user is not coaching this trainee")
         else:
             self.trainees.remove(trainee)
+            if not auto:
+                trainee.removeCoach(self, True)
+
+    def __str__(self):
+        n = "Name: " + self.name
+        c = " Coach: " + ', '.join([x.name for x in self.coaches])
+        t = " Trainees: " + ', '.join([x.name for x in self.trainees])
+        v = " Version: " + str(self.version)
+        return n + c + t + v
